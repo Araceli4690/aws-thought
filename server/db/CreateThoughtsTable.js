7ca4bf8517e2bfa6279564e61aed9844ac7d693c
araceli <carmona.araceli.4690@gmail.com>
2021-11-08 20:15:25 -0800

    creating params object

--- server/db/CreateThoughtsTable.js
+++ server/db/CreateThoughtsTable.js
@@ -10,3 +10,20 @@ AWS.config.update({
 //DynamoDB service object
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
+//creating params object
+const params = {
+    TableName: "Thoughts",
+    KeySchema: [
+        //keys indicate properties and values indicate the schema
+        { AttributeName: "username", KeyType: "HASH" }, //partition key
+        { AttributeName: "createdAt", KeyType: "RANGE" } //sort key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: "username", AttributeType: "S" },
+        { AttributeName: "createdAt", AttributeType: "N" }
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
